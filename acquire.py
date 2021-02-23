048a8fe88edfad7ce25a2352b824ee1af985bf65
Justin Sullivan <sullivan.justin.michael@gmail.com>
2021-02-23 09:32:14 -0600

    completed data acquisition exercises

--- acquire.py
+++ acquire.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import os
 
 from env import host, password, user
 
@@ -10,12 +11,50 @@ def get_connection(db, user=user, host=host, password=password):
     '''
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
-def get_titanic_data():
+ def get_titanic_data():
     '''
-    This function reads in the titanic data from the CodeUp db
-    and returns a pandas DF with all columns
+    This function reads in titanic data from Codeup database and writes data to
+    a csv file if cached == False or if cached == True reads in titanic df from
+    a csv file, returns df.
     '''
-    sql_query = 'SELECT * FROM passengers'
-    return pd.read_sql(sql_query, get_connection('titanic_db'))
+    filename = 'titanic.csv'
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        # Read fresh data from db into a DataFrame.
+        df = pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
+        
+        # Write DataFrame to a csv file.
+        df.to_csv(filename)
+          
+    return df
 
-    
+
+get_titanic_data().head()  
+
+def get_iris_data():
+    '''
+    This function reads in iris data from Codeup database and writes data to
+    a csv file if cached == False or if cached == True reads in iris df from
+    a csv file, returns df
+    '''
+
+    filename = 'iris.csv'
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        
+        # Read fresh data from db into a DataFrame. Joins species id on dataframe. 
+        df = pd.read_sql('''
+    SELECT * FROM measurements as m
+    RIGHT JOIN species ON m.`species_id`
+    ''', get_connection('iris_db'))
+        
+        # Write DataFrame to a csv file.
+        df.to_csv(filename)
+          
+    return df
+
+get_iris_data().head()
+
+
