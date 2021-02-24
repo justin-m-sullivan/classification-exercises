d9abaca8ac2cd33b07a46be6af6a2d45f66b6cc2
Justin Sullivan <sullivan.justin.michael@gmail.com>
2021-02-24 10:13:01 -0600

    completed prepare exercises

--- prepare.py
+++ prepare.py
@@ -23,19 +23,31 @@ from pydataset import data
 
 df = acquire.get_iris_data()
 
+def clean_iris(df):
+    '''
+    clean_iris takes in the iris data set and prepares it for analysis by:
+    dropping columns: measurement_id, species_id
+    renaming columns: species_name == species
+    encoding columns: species
+    '''
+    dropcols = ['species_id', 'measurement_id', 'Unnamed: 0', 'species_id.1']
+    df.drop(columns=dropcols, inplace=True)
+    df.rename(columns={'species_name': 'species'}, inplace=True)
+    dummies = pd.get_dummies(df[['species']])
+    return pd.concat([df, dummies], axis=1)
+
 def prep_iris(df):
     '''
     prep_iris takes in the iris data set and prepares it for analysis by:
     dropping columns: measurement_id, species_id
     renaming columns: species_name == species
     encoding columns: species
-    '''
-    dropcols = ['species_id', 'measurement_id']
-    df.drop(columns=dropcols, inplace=True)
-    df.rename(columns={'species_name': 'species'}, inplace=True)
-    dummies = pd.get_dummies(df[['species']], drop_first=True)
-    return pd.concat([df, dummies], axis=1)
     
+    performs a train, validate, and test split 
+    return three pandas dataframes for train, validate, split
+    '''
+    df = clean_iris(df)
+    train_validate, test = train_test_split(df, test_size=0.2, random_state=1776, stratify=df.species)
+    train, validate = train_test_split(train_validate, train_size=0.7, random_state=1776, stratify=train_validate.species)
+    return train, validate, test
 
-
-prep_iris(df)
