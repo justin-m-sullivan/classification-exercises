3451a8ba3147baca09e2b7351dcc7a688167ac8c
Justin Sullivan <sullivan.justin.michael@gmail.com>
2021-02-25 15:54:18 -0600

    updating prepare with data split fx

--- prepare.py
+++ prepare.py
@@ -51,3 +51,18 @@ def prep_iris(df):
     train, validate = train_test_split(train_validate, train_size=0.7, random_state=1776, stratify=train_validate.species)
     return train, validate, test
 
+def split(df, stratify_by=None):
+    """
+    Crude train, validate, test split
+    To stratify, send in a column name
+    """
+    
+    if stratify_by == None:
+        train, test = train_test_split(df, test_size=.3, random_state=123)
+        train, validate = train_test_split(df, test_size=.3, random_state=123)
+    else:
+        train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[stratify_by])
+        train, validate = train_test_split(df, test_size=.3, random_state=123, stratify=train[stratify_by])
+    
+    return train, validate, test
+
